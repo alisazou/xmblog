0fde2dc5d39aaf9145bbbf57c7e7b2e314026a21
alisazou <513242026@qq.com>
2016-11-10 18:33:29 +0800

    xmblog

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+
+/*
+req.query: 解析后的 url 中的 querystring，如 ?name=haha，req.query 的值为 {name: 'haha'}
+req.params: 解析 url 中的占位符，如 /:name，访问 /haha，req.params 的值为 {name: 'haha'}
+req.body: 解析后请求体，需使用相关的模块，如 body-parser，请求体为 {"name": "haha"}，则 req.body 为 {name: 'haha'}
+
+*/
+
+
+
+
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
+  // 404 page
+  app.use(function (req, res) {
+	if (!res.headersSent) {
+	    res.render('404');
+	  }
+	});
+};
