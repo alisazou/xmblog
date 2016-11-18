dc88a41569a0d9fac27cc26f01d65a906e6e5d9f
alisazou <513242026@qq.com>
2016-11-18 21:02:36 +0800

    添加表单输入

---
+++ public/js/controllers.js
@@ -0,0 +1,21 @@
+
+
+
+function formCtrl($scope) {
+  $scope.names = ["Google", "Runoob", "Taobao"];
+}
+
+function SelectProject($scope) {
+  $scope.names = ["Google", "Runoob", "Taobao"];
+}
+function SelectJiaohu($scope) {
+  $scope.names = ["Google", "Runoob", "Taobao"];
+}
+function SelectShiJue($scope) {
+  $scope.names = ["Google", "Runoob", "Taobao"];
+}
+
+function SelectUi($scope) {
+  $scope.names = ["Google", "Runoob", "Taobao"];
+}
+
