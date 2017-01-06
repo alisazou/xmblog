dd8645d16b04c6b42134bf75250c2b469474b0c1
alisazou <513242026@qq.com>
2017-01-06 19:02:57 +0800

    添加日期功能

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,16 +1,34 @@
 
 
-function SelectProject($scope) {
-  $scope.names = ["张三", "李四", "王五"];
-}
-function SelectJiaohu($scope) {
-  $scope.names = ["aster", "niko", "tang"];
-}
-function SelectShiJue($scope) {
-  $scope.names = ["gaby", "zhiyi", "庐山"];
-}
 
-function SelectUi($scope) {
-  $scope.names = ["alisa", "huzeng", "wenjun"];
-}
+
+(function (angular) {
+  'use strict';
+
+  var app = angular.module('appAdd', []);
+
+    	app.controller('selectControllerPm', function ($scope) {
+            $scope.mycity = '张三';
+            $scope.Cities = [{ id: 1, name: '李四' }, { id: 2, name: '王五' }, { id: 3, name: '张三' }];
+        });
+        app.controller('selectControllerInteractive', function ($scope) {
+            $scope.mycity = 'aster';
+            $scope.Cities = [{ id: 1, name: 'aster' }, { id: 2, name: 'niko' }, { id: 3, name: 'tang' }];
+        });
+        app.controller('selectControllerDesign', function ($scope) {
+            $scope.mycity = 'gaby';
+            $scope.Cities = [{ id: 1, name: 'gaby' }, { id: 2, name: 'zhiyi' }, { id: 3, name: '庐山' }];
+        });
+        app.controller('selectControllerUi', function ($scope) {
+            $scope.mycity = 'alisa';
+            $scope.Cities = [{ id: 1, name: 'huzeng' }, { id: 2, name: 'wenjun' }, { id: 3, name: 'alisa' }];
+        });
+
+        
+   
+}(angular));
+
+
+
+
 
